defee49195a28006fcdea7744ef34af442af12e6
Kleyton Matos Ramos <kleytonramos@OPORTUNIDADES.es>
2019-03-20 18:19:19 -0300

    add ods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from export import Crede
 from processing import Processing
 
-ex = Crede()
-ex.map()
+pro = Processing()
+pro.map() 
 
 
--- processing.py
+++ processing.py
@@ -1,7 +1,6 @@
 from os.path import isfile, join, basename
 import os, json, shutil
 import pandas as pd
-import xlwt, ezodf
 from lxml import etree
 
 class Processing:
@@ -9,6 +8,23 @@ class Processing:
     pass
   
   # Lista todos os diretórios
+  def ods(self, dir):
+    for current_file in os.listdir(dir):
+      if current_file.endswith("xls") or current_file.endswith("xlsx"):
+        print(dir + '/' + current_file)
+        try:
+          file = pd.ExcelFile(dir + '/' + current_file)
+          writer = pd.ExcelWriter(dir + '/' + current_file)
+          print(current_file)
+          for x in file.sheet_names:
+            df = pd.read_excel(file, sheet_name=x)
+            df['nome_original_' + 'CREDE 13'] = current_file
+            df.to_excel(writer, x)
+            writer.save()
+        except:
+          print('Error -------> ' + current_file)
+          shutil.move(dir + '/' + current_file, 'CREDE 13/bases não extraidas/' + current_file)
+  
   def read_dir(self, dir):
     for current_file in os.listdir(dir):
       if current_file.endswith("xls") or current_file.endswith("xlsx"):
@@ -23,28 +39,14 @@ class Processing:
             writer.save()
         except:
           print('Error -------> ' + current_file)
-          shutil.move(dir + '/' + current_file, dir + '/bases não extraidas/' + current_file)
-
-  def list_dir(self):
-    list_files = []
-    writer = pd.ExcelWriter('out.xlsx')
-    
-    for current_file in os.listdir('CREDE 30/bases extraidas'):
-      if current_file.endswith("xls") or current_file.endswith("xlsx"):
-        list_files.append(current_file)
-      
-    df = pd.DataFrame(data=list_files)
-    df.to_excel(writer,'CREDE 30')
-    writer.save()
 
  # Percorre todos os diretórios e executa todas as funções. 
   def map(self):
-    for folder in os.listdir('.'):
-      try:
-        print(folder)
-        self.read_dir(folder)
-      except:
-        print('Non-numeric data found in the file.')
+    # self.ods('CREDE 13/Exrtação ods')
+    print('CREDE 13')
+    self.read_dir('CREDE 13')
+      
+      
   
   
     
